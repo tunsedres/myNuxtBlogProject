70c49f2955019f084a5b55bd3bd179e1cb6f8050
Tuncay Elv <tunsedres@gmail.com>
2020-12-08 23:32:50 +0300

    second commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'image/x-icon', href: 'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap' }
     ]
   },
 
